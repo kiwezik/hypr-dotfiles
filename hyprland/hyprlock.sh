3a812c164a1232aa6e6afd0868ca22726ef5af4a
kiwezik <kiwezik@outlook.com>
2025-09-27 13:34:16 +0300

    +350mb ram

--- hyprland/hyprlock.sh
+++ hyprland/hyprlock.sh
@@ -2,4 +2,4 @@
 WALLPAPER_PATH=$(swww query | grep "DP-1" | awk -F'image: ' '{print $2}')
 ESCAPED_PATH=$(printf '%s\n' "$WALLPAPER_PATH" | sed 's/[\/&]/\\&/g')
 sed -i "s|^\([[:space:]]*path = \).*|\1$ESCAPED_PATH|" ~/hypr-dotfiles/hyprland/hyprlock.conf
-hyprlock --immediate-render -c ~/hypr-dotfiles/hyprland/hyprlock.conf
+hyprlock -c ~/hypr-dotfiles/hyprland/hyprlock.conf
